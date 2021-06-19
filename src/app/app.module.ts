52b6435e36bf911842ae0cca74c300f31f41ef1e
Molchanova Tatyana <mtuspeh@gmail.com>
2021-06-19 19:38:16 +0300

    npm run prerender leads to error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RendererModule, TransferHttpCacheModule } from '@nguniversal/common/clover';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'appId' }),
+    AppRoutingModule,
+    RendererModule.forRoot(),
+    TransferHttpCacheModule
   ],
   providers: [],
   bootstrap: [AppComponent]
